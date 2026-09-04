c7e43acb91ad514fa1b3706450e6e003afb7e5f3
ltshivam5516-ctrl <lt.shivam5516@gmail.com>
2026-09-05 01:11:29 +0530

    Add files via upload

---
+++ even_odd.cpp
@@ -0,0 +1,22 @@
+{
+	int a,even,odd;
+	printf("enter the value of a");
+	scanf("%d", &a);
+	
+	if (a%2==0){
+		
+		printf("even");
+		
+	}
+	   else {
+	   	
+	   	printf("odd");
+	   	
+	   	
+	   	
+	   	
+	   }
+	return 0;
+	
+}
+
