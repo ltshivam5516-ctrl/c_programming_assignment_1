25bb3523111857099bf05a947daa02d195cd79d0
ltshivam5516-ctrl <lt.shivam5516@gmail.com>
2026-09-05 01:10:11 +0530

    Add files via upload

---
+++ three_nu_bigger_without_special_operator.cpp
@@ -0,0 +1,57 @@
+#include <stdio.h>
+int main (){
+	
+	int a,b,c;
+	printf("enter the value of a,b,c");
+	scanf("%d %d %d", &a,&b,&c);
+	
+	
+	if (a>b){
+		
+		if (a>c){
+			
+		printf ("a");}
+			
+		else {  
+		printf("c");
+		
+		}		}
+
+	else {
+	if 	(b>c){
+		printf("b");}
+		
+		else {
+			printf("c");
+			
+			
+		}
+			
+			
+			
+			
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+	
+	
+
+     
+     
+     return 0;
+     
+	
+	
+	
+	
+	
+	
+	
+	
+}
