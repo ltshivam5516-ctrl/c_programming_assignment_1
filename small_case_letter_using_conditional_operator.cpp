9d65d59bdb4de21bb1a427b699310ab995b34929
ltshivam5516-ctrl <lt.shivam5516@gmail.com>
2026-09-05 10:58:48 +0530

    Add files via upload

---
+++ small_case_letter_using_conditional_operator.cpp
@@ -0,0 +1,33 @@
+#include <stdio.h>
+int main (){
+	
+	char ch;
+	printf("enter the value of ch");
+	scanf(" %c", &ch);
+	
+	(ch>='a' && ch<='z')?
+	printf("small case letter"):
+	printf("not small case letter ");
+	
+	
+	return 0;
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
