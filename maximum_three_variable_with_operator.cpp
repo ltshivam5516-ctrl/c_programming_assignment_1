1028c5bbb99c1563856167b1f840dcca802ec562
ltshivam5516-ctrl <lt.shivam5516@gmail.com>
2026-09-05 09:57:28 +0530

    Add files via upload

---
+++ maximum_three_variable_with_operator.cpp
@@ -0,0 +1,38 @@
+#include <stdio.h>
+int main ()
+
+{
+	int a,b,c;
+	printf("enter the value of a,b,c");
+	scanf("%d %d %d", &a,&b,&c);
+	
+	if (a>=b && a>=c){
+		
+		printf("a");
+	}
+	
+	else if (b>=c && b>=a){
+		
+		printf("b");
+		
+		
+	}
+	
+	else 
+	printf("c");
+	
+	
+	
+	return 0;
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
